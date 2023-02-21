e038bd25e813e3d81ebae0786b1f042a2ae18d04
Franco Garcia <garciacfranco2001@gmail.com>
2023-02-21 20:49:56 -0300

    working with operators in python

---
+++ operadores/op_comparacion.py
@@ -0,0 +1,14 @@
+#Permiten comparar valores, devuelven True o False
+es_igual_que = 5 == 6
+
+es_distinto_que = 5 == 6
+
+mayor_que = 5 > 6
+
+mayor_o_igual = 5 >= 6
+
+menor_que = 5 < 6
+
+menor_o_igual = 5 <= 6
+
+print(es_igual_que)
---
+++ operadores/operadores.py
@@ -0,0 +1,29 @@
+#suma y resta (+ y -)
+suma = 12 + 5
+resta = 12 - 5
+
+#multiplicacion y división (* y /)
+multiplicacion = 12 * 5
+division = 12 / 5 #devuelve un dato float (flotante), aunque el resultado sea un entero
+
+#potenciación (exponenete) (**)
+exponente = 12**5
+
+#división baja (//)
+division_baja = 12//5
+
+#modulo o resto (%)
+modulo = 12 % 5
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(exponente)
+print(division_baja) #devuelve un entero redondeando hacia abajo
+print(modulo)
+
+#type nos devuelve el tipo de dato
+tipo_de_dato = type(suma)
+
+
